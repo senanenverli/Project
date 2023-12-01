615996d51fbcf7428a685b829d735440815d8749
unknown <senanaal@code.edu.az>
2023-12-01 20:53:12 +0400

    c#_project

---
+++ CustomList.cs
@@ -0,0 +1,103 @@
+﻿public class CustomList<T>
+{
+    public T[] List;
+    private int count;
+    private int capacity;
+
+    public int Count 
+    { 
+        get =>  count;  
+    }
+    public int Capacity 
+    {
+        get => capacity; 
+    }
+
+    public CustomList( )
+    {
+        List = new T[0];
+        count = 0;
+        capacity = 0;
+    }
+
+    public void Add(T item)
+    {
+        if (count == capacity)
+        {
+            int newCapacity = capacity == 0 ? 4 : capacity * 2;
+            Array.Resize(ref List, newCapacity);
+            capacity = newCapacity;
+        }
+
+        List[count] = item;
+        count++;
+    }
+
+    public T Find(Predicate<T> match)
+    {
+        foreach (T item in List)
+        {
+            if (match(item))
+            {
+                return item;
+            }
+        }
+
+        throw new InvalidOperationException("No matching element found."); ;
+    }
+
+    public List<T> FindAll(Predicate<T> match)
+    {
+        List<T> resultList = new List<T>();
+
+        foreach (T item in List)
+        {
+            if (match(item))
+            {
+                resultList.Add(item);
+            }
+        }
+
+        return resultList;
+    }
+
+    public bool Contains(T item)
+    {
+        return Array.IndexOf(List, item) != -1;
+    }
+
+    public bool Exists(Predicate<T> match)
+    {
+        foreach (T item in List)
+        {
+            if (match(item))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void Remove(T item)
+    {
+        int index = Array.IndexOf(List, item);
+
+        if (index != -1)
+        {
+            for (int i = index; i < count - 1; i++)
+            {
+                List[i] = List[i + 1];
+            }
+
+            count--;
+        }
+    }
+    public IEnumerable<T> GetAllItems()
+    {
+
+        return List.Take(count);
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿CustomList<int> List = new CustomList<int>();
+List.Add(1);
+List.Add(2);
+List.Add(3);
+List.Add(4);
+List.Add(5);
+Console.WriteLine( "Count =>" + List.Count);
+Console.WriteLine("Capacity => " + List.Capacity); 
+Console.WriteLine("contains => "+List.Contains(2));
+List.Remove(2);
+Console.WriteLine("Count =>" + List.Count);
+bool exists = List.Exists(x => x == 3);
+Console.WriteLine("exits => " + exists);
+Console.WriteLine("Capacity => " + List.Capacity);
+
+foreach (var item in List.GetAllItems())
+{
+    Console.WriteLine("Items =>:" + item);
+}
